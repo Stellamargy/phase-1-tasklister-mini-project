fa5f76e5194490bb2926dbc7db73ad28c5dd1663
Stellamargy <stella.oloo@student.moringaschool.com>
2022-12-04 23:06:49 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const myToDo = document.querySelector("#tasks");
+  const newTaskdescribed = document.querySelector("#new-task-description");
+  const taskForm = document.querySelector("#create-task-form");
+
+  const validInput = (input) => {
+    input = input.trim();
+    return input ? input : false;
+  };
+
+  const toAddTask = (event) => {
+    event.preventDefault();
+    toGetTask();
+  };
+
+  const toGetTask = () => {
+    const task = newTaskdescribed.value;
+    if (validInput(task)) {
+      creatingTask(task);
+    }
+  };
+
+  const creatingTask = (taskContent) => {
+    const task = document.createElement("li");
+    task.textContent = taskContent;
+    myToDo.appendChild(task);
+    taskForm.reset();
+  };
+
+  taskForm.addEventListener("submit", toAddTask);
 });
